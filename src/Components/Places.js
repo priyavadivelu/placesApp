0632bd3ecc2611444868e22472465c456a561ee8
priyavadivelu <42160349+priyavadivelu@users.noreply.github.com>
2021-02-15 11:21:54 -0500

    Update Places.js

--- src/Components/Places.js
+++ src/Components/Places.js
@@ -5,26 +5,7 @@ import { Link, Router } from "react-router-dom";
 import { useEffect } from "react";
 
 function Places(props) {
-  //   const displayPhotos = () => {
-  //     return props.post.map((items) => {
-  //       return (
-  //         <section>
-  //           <img id="logo_img" src={items.logo} alt="business logo" />
-  //         </section>
-  //       );
-  //     });
-  //   };
-  //   return (
-  //     <Grid
-  //       container
-  //       spacing={24}
-  //       direction="column"
-  //       alignItems="center"
-  //       justify="center"
-  //     >
-  //       {displayPhotos()}
-  //     </Grid>
-  //   );
+ 
   const {
     location: {
       state: { data },
@@ -44,7 +25,7 @@ function Places(props) {
         <Grid item xs={4} sm={4}>
           <img id="img-logo" src={data.logo} alt="business logo" />
         </Grid>
-        <Grid item xs={4} sm={4} id="places_details">
+        <Grid item xs={5} sm={5} id="places_details">
           <div className="name">
             <b>Name:</b>
             {data.businessName}
